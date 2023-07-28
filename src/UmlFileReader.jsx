2e1717e97b10d620035088e1a27be1981aa4a7f2
bhushan6 <bhushanwagh6@gmail.com>
2023-07-28 16:39:04 +0530

    clean up

--- src/UmlFileReader.jsx
+++ src/UmlFileReader.jsx
@@ -1,38 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-const parser = new DOMParser();
-
-function traverseNode(node, parent) {
-  const nodeJson = {
-    node: undefined,
-    attributes: {},
-    children: [],
-  };
-  if (node.nodeType === 1) {
-    nodeJson.node = node.nodeName;
-    if (node.attributes.length > 0) {
-      for (let i = 0; i < node.attributes.length; i++) {
-        const attribute = node.attributes[i];
-        nodeJson.attributes[attribute.nodeName] = attribute.nodeValue;
-      }
-    }
-    // tree[nodeJson.attributes["xmi:id"]] = nodeJson;
-    if (parent) {
-      parent.children.push(nodeJson);
-    }
-    const children = node.childNodes;
-    for (let i = 0; i < children.length; i++) {
-      traverseNode(children[i], nodeJson);
-    }
-
-    return nodeJson;
-  }
-}
+import { parser, traverseNode } from "./utils";
 
 export const UMLFileReader = ({ setUMLText }) => {
-  //   const [umlText, setUMLText] = useState("");
-
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -57,8 +28,6 @@ export const UMLFileReader = ({ setUMLText }) => {
       reader.readAsText(file);
     }
   };
-
-  //   console.log(umlText);
 
   return (
     <div
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import actorImg from "./assets/actor.png";
 
-export const interfaceCreator = {
+const interfaceCreator = {
   Package: (diagram, text, key, parent) => {
     diagram.model.addNodeData({
       key: key,
@@ -76,3 +76,113 @@ export const templateCreator = (diagram, templatesMap) => {
     });
   });
 };
+
+const cleanNode = ({ diagram, node }) => {
+  diagram.model.removeNodeData(node);
+  diagram.updateAllTargetBindings();
+};
+
+const cleanLink = ({ diagram, link }) => {
+  diagram.model.removeLinkData(link);
+  diagram.updateAllTargetBindings();
+};
+
+export const createGoJsGraphics = ({ node, parent, diagram }) => {
+  if (node.node === "packagedElement") {
+    const type = node.attributes["xmi:type"].split(":");
+    const category = type[type.length - 1];
+    if (category === "Association") {
+      const ends = [];
+      const label = node.attributes.name || "";
+
+      node.children.forEach((child) => {
+        if (child.node === "ownedEnd") {
+          ends.push(child.attributes.type);
+        }
+      });
+
+      interfaceCreator[category] &&
+        interfaceCreator[category](diagram, ends[1], ends[0], label);
+
+      const linkData = diagram.model.linkDataArray.find(function (link) {
+        return link.from === ends[1] && link.to === ends[0];
+      });
+
+      return () => {
+        cleanLink({ link: linkData, diagram });
+      };
+    } else {
+      interfaceCreator[category] &&
+        interfaceCreator[category](
+          diagram,
+          node.attributes.name || category,
+          node.attributes["xmi:id"],
+          parent.attributes["xmi:id"]
+        );
+
+      const nodeData = diagram.model.findNodeDataForKey(
+        node.attributes["xmi:id"]
+      );
+      return () => {
+        cleanNode({ node: nodeData, diagram });
+      };
+    }
+  } else if (node.node === "include") {
+    const type = node.attributes["xmi:type"].split(":");
+    const category = type[type.length - 1];
+    const from = parent.attributes["xmi:id"];
+    const to = node.attributes["addition"];
+    interfaceCreator[category] && interfaceCreator[category](diagram, from, to);
+
+    const linkData = diagram.model.linkDataArray.find(function (link) {
+      return link.from === from && link.to === to;
+    });
+
+    return () => {
+      cleanLink({ link: linkData, diagram });
+    };
+  } else if (node.node === "extend") {
+    const type = node.attributes["xmi:type"].split(":");
+    const category = type[type.length - 1];
+    const to = node.attributes["extendedCase"];
+    const from = parent.attributes["xmi:id"];
+
+    interfaceCreator[category] && interfaceCreator[category](diagram, from, to);
+
+    const linkData = diagram.model.linkDataArray.find(function (link) {
+      return link.from === from && link.to === to;
+    });
+
+    return () => {
+      cleanLink({ link: linkData, diagram });
+    };
+  }
+};
+
+export function traverseNode(node, parent) {
+  const nodeJson = {
+    node: undefined,
+    attributes: {},
+    children: [],
+  };
+  if (node.nodeType === 1) {
+    nodeJson.node = node.nodeName;
+    if (node.attributes.length > 0) {
+      for (let i = 0; i < node.attributes.length; i++) {
+        const attribute = node.attributes[i];
+        nodeJson.attributes[attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+    if (parent) {
+      parent.children.push(nodeJson);
+    }
+    const children = node.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      traverseNode(children[i], nodeJson);
+    }
+
+    return nodeJson;
+  }
+}
+
+export const parser = new DOMParser();
